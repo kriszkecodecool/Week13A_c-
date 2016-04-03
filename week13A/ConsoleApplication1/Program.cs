52e188182637edb94827042e3aebe97aa020c3ab
kriszkecodecool <kriszke20@gmail.com>
2016-04-04 00:50:31 +0200

    Uploading my files.

---
+++ week13A/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Kovacs = new Employee("Géza", DateTime.Now, 1000, "léhűtő");
+            Kovacs.Room = new Room(111);
+            Employee Kovacs2 = (Employee)Kovacs.Clone();
+            Kovacs2.Room.Number = 112;
+
+
+            Console.WriteLine(Kovacs.ToString());
+            Console.WriteLine(Kovacs2.ToString());
+            Console.ReadKey();
+        }
+    }
+    }
---
+++ week13A/ConsoleApplication1/Room.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Room
+    {
+
+            private int number;
+
+            public int Number
+            {
+                get { return number; }
+                set { number = value; }
+            }
+
+            public Room(int number)
+            {
+                this.number = number;
+            }
+    }
+}
